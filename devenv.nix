0073a1622d8f6ef5a674a462f3fc9d703760c52a
Ellian Carlos <elliancarlos@gmail.com>
2025-01-18 00:29:42 -0300

    feat: add devenv

---
+++ devenv.nix
@@ -0,0 +1,35 @@
+{ pkgs, lib, config, inputs, ... }:
+{
+  env.GREET = "devenv";
+
+  packages = [
+    pkgs.git
+    pkgs.ollama
+  ];
+
+  scripts.hello.exec = "echo hello from $GREET";
+
+  enterShell = ''
+    hello
+    git --version
+  '';
+
+  enterTest = ''
+    echo "Running tests"
+    git --version | grep "2.42.0"
+  '';
+  languages.python = {
+    enable = true;
+    venv.enable = true;
+    venv.requirements = ''
+      ollama
+      crewai
+      unstructured
+      langchain-community
+      Jinja2>=3.1.2
+      click>=7.0
+      duckduckgo-search
+    '';
+    uv.enable = true;
+  };
+}
